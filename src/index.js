e9ee27634afaeeb9d9c485c6bf5a40e76c0b0dc8
eldr0n-git <eldron@gmail.com>
2021-08-18 20:00:57 +0600

    init app

--- src/index.js
+++ src/index.js
@@ -37,8 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    let decodedWord = [];
+    for (let i = 0; i < expr.length; i = i + 10){
+        let encodedLetter = expr.slice(i, i + 10);
+        let ltrx = [];
+        let y = 0;
+
+        if( encodedLetter[0] !== '*'){
+            for (let j = 0; j < encodedLetter.length; j = j + 2){
+                if(encodedLetter[j] === '1' && encodedLetter[j+1] === '0'){
+                    ltrx[y] = '.';
+                    y++;
+                } else if(encodedLetter[j] === '1' && encodedLetter[j+1] === '1'){
+                    ltrx[y] = '-';
+                    y++;
+                }   
+            }
+            let key = ltrx.toString().replace(/,/g, '');
+            decodedWord.push(MORSE_TABLE[key]);
+        } else {
+            decodedWord.push(' ');
+        }
+    }
+    return decodedWord.toString().replace(/,/g, '');
 }
 
 module.exports = {
